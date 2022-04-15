6c4bdd65695684cb85bf95a4fa30096b0840ac35
DESKTOP-0LUN99P\parkm <parkmansoo@nate.com>
2022-04-15 16:57:41 +0900

    4조 화이팅 !!

---
+++ src/main/java/com/hanghea/clonecarrotbe/CloneCarrotBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghea.clonecarrotbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCarrotBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneCarrotBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghea/clonecarrotbe/CloneCarrotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghea.clonecarrotbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCarrotBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
